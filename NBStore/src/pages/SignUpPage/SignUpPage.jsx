8d7545aa9218b7b83860af6b3503b1bcd2dd8f33
“maiduytuanminh” <maiduytuanminh@gmail.com>
2025-11-30 21:44:21 +0700

    Update 30/11

--- NBStore/src/pages/SignUpPage/SignUpPage.jsx
+++ NBStore/src/pages/SignUpPage/SignUpPage.jsx
@@ -125,7 +125,7 @@ const SignUpPage = () => {
         </WrapperContainerLeft>
         <WrapperContainerRight>
           <Image src={imageLogo} preview={false} alt="iamge-logo" height="203px" width="203px" />
-          <h4>Mua sắm tại LTTD</h4>
+          <h4>Mua sắm tại NB</h4>
         </WrapperContainerRight>
       </div>
     </div >
